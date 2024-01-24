ac407f5096da611cf47ac6f08c6ba3a2e8195ba0
Felix <felixwhman@gmail.com>
2024-01-24 09:05:08 -0500

    updated version

--- script.js
+++ script.js
@@ -7,15 +7,17 @@
 
 */
 //set global variables
-let button = document.querySelector('#searchButton')
+const button = document.querySelector('#searchButton')
+const pokemonContainer = document.getElementById('pokemonContainer')
 //where does this need to be scoped?
 
 //Axios call goes here
 //remember to use Async and Await!
-
 button.addEventListener('click', async () => {
 	let pokemonName = document.querySelector('#pokemonName')
 	let pokemonImage = document.querySelector('#pokemonImage')
+	let pokemonHeight = document.querySelector('#pokemonHeight')
+	let pokemonTypes = document.querySelector('#pokemonTypes')
 	let textInput = document.querySelector('#inputBar').value
 	console.log(textInput)
 
@@ -23,14 +25,43 @@ button.addEventListener('click', async () => {
 	//remember to use Async and Await!
 	let result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${textInput}`)
 	console.log(result.data)
-
+	pokemonContainer.style.display = 'block'
 	//DOM Setters go here
-	if (result && result.data.sprites && result.data.sprites.front_default) {
-		pokemonName.textContent = result.data.name
-		pokemonImage.src = result.data.sprites.front_default
+	if (
+		result &&
+		result.data.sprites &&
+		result.data.sprites.other &&
+		result.data.sprites.other.showdown &&
+		result.data.sprites.other.showdown.front_default
+	) {
+		pokemonName.textContent =
+			result.data.name.charAt(0).toUpperCase() + //Will make first letter of name Upper case
+			result.data.name.slice(1).toLowerCase() //Rest of the name will be lower case
+		pokemonImage.setAttribute(
+			'src',
+			result.data.sprites.other.showdown.front_default
+		)
 	} else {
-		console.log('Data not available')
-		pokemonName.textContent = 'Image not available'
-		pokemonImage.src = ''
+		console.log('Pokemon not found')
+		pokemonName.textContent = 'Pokemon not found'
+		pokemonImage.setAttribute('src', '')
+	}
+
+	if (result.data.game_indices) {
+		pokemonHeight.textContent = 'Height: ' + result.data.height + `'0"`
+		console.log(result.data.height)
+	}
+
+	if (result.data.types && result.data.types.length > 0) {
+		let numTypes = 'Type(s): '
+		for (let i = 0; i < result.data.types.length; i++) {
+			numTypes += result.data.types[i].type.name
+			if (i < result.data.types.length - 1) {
+				numTypes += ',  '
+			}
+		}
+		pokemonTypes.textContent = numTypes
+	} else {
+		pokemonTypes.textContent = 'Type(s): Not Available'
 	}
 })
